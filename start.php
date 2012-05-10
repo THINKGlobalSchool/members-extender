f5642d655186accbff43dfe29b8abffcdea63faa
Jeff Tilson <jrtilson@gmail.com>
2012-05-10 13:49:39 -0400

    Initial commit

---
+++ start.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Members-Extender start.php
+ * 
+ * @package Members-Extender
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010 - 2012
+ * @link http://www.thinkglobalschool.com/
+ * 
+ * OVERRIDES:
+ *
+ */
+
+// Register init
+elgg_register_event_handler('init', 'system', 'members_extender_init');
+
+// Init
+function members_extender_init() {	
+	
+}
