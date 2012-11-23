7b84a63d052707c0cca5e2025d2ffec7247f229d
Jeff Tilson <jtilson@thinkglobalschool.com>
2012-11-23 11:17:47 -0500

    Only displaying students, staff and teacher tabs if roles are defined in plugin settings

--- views/default/members-extender/navigation/tabs.php
+++ views/default/members-extender/navigation/tabs.php
@@ -13,21 +13,27 @@
 $custom_selected = get_input('members_custom_tab_selected');
 // Tack these tabs on the to the tab array if under members/schools context
 if (elgg_get_context() == 'members' || elgg_get_context() == 'schools') {
-	$vars['tabs']['students'] = array(
-		'title' => elgg_echo('members-extender:label:students'),
-		'url' => "members/students",
-		'selected' => $custom_selected == 'students',
-	);
+	if (elgg_get_plugin_setting('student_role', 'members-extender')) {
+		$vars['tabs']['students'] = array(
+			'title' => elgg_echo('members-extender:label:students'),
+			'url' => "members/students",
+			'selected' => $custom_selected == 'students',
+		);
+	}
 	
-	$vars['tabs']['teachers'] = array(
-		'title' => elgg_echo('members-extender:label:teachers'),
-		'url' => "members/teachers",
-		'selected' => $custom_selected == 'teachers',
-	);
+	if (elgg_get_plugin_setting('teacher_role', 'members-extender')) {
+		$vars['tabs']['teachers'] = array(
+			'title' => elgg_echo('members-extender:label:teachers'),
+			'url' => "members/teachers",
+			'selected' => $custom_selected == 'teachers',
+		);
+	}
 
-	$vars['tabs']['staff'] = array(
-		'title' => elgg_echo('members-extender:label:staff'),
-		'url' => "members/staff",
-		'selected' => $custom_selected == 'staff',
-	);
+	if (elgg_get_plugin_setting('staff_role', 'members-extender')) {
+		$vars['tabs']['staff'] = array(
+			'title' => elgg_echo('members-extender:label:staff'),
+			'url' => "members/staff",
+			'selected' => $custom_selected == 'staff',
+		);
+	}
 }
